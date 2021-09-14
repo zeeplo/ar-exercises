b03c59b4f95a8b2390d03790da415915a1fd061d
William Ye <w3ye@pm.me>
2021-09-13 23:45:29 -0400

    add exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
+    has_many :employees
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: {greater_than: 0}
+    validate :at_least_one_apparel
 
+    def at_least_one_apparel
+        if mens_apparel == false && womens_apparel == false
+            errors.add(:id, "mens apparel and womens apparel can't both be false")
+        end
+    end
 end
